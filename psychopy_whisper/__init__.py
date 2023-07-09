f88474e23f581eae366450eb44d576e6c193f43a
Matthew D. Cutone <mcutone@opensciencetools.org>
2023-07-09 13:00:10 -0400

    FF: Model downloading

--- psychopy_whisper/__init__.py
+++ psychopy_whisper/__init__.py
@@ -8,59 +8,14 @@
 """Speech-to-text transcription using OpenAI Whisper.
 """
 
-__version__ = '0.0.1'
+__version__ = '0.0.1'  # plugin version
 
+import os
+import psychopy.logging as logging
+from psychopy.preferences import prefs
+from psychopy.sound.audioclip import AudioClip
 from psychopy.sound.transcribe import TranscriptionResult, BaseTranscriber
-
-
-def _download(url, root, in_memory):
-    """Download a model for the OpenAI Whisper speech-to-text transcriber.
-
-    This function is monkey-patched to override the `_download()` function to 
-    use the `requests` library to get models from remote sources. This gets 
-    around the SSL certificate errors we see with `urllib`.
-
-    """
-    # derived from source code found here:
-    # https://github.com/openai/whisper/blob/main/whisper/__init__.py
-    import hashlib
-    import os
-    import requests
-    import warnings
-
-    os.makedirs(root, exist_ok=True)
-
-    expected_sha256 = url.split("/")[-2]
-    download_target = os.path.join(root, os.path.basename(url))
-
-    if os.path.exists(download_target) and not os.path.isfile(download_target):
-        raise RuntimeError(
-            f"{download_target} exists and is not a regular file")
-
-    if os.path.isfile(download_target):
-        with open(download_target, "rb") as f:
-            model_bytes = f.read()
-        if hashlib.sha256(model_bytes).hexdigest() == expected_sha256:
-            return model_bytes if in_memory else download_target
-        else:
-            warnings.warn(
-                f"{download_target} exists, but the SHA256 checksum does not "
-                f"match; re-downloading the file"
-            )
-    
-    # here is the change we make that uses `requests` instead of `urllib`
-    req = requests.get(url, allow_redirects=True)
-    with open(download_target, 'wb') as dt:
-        dt.write(req.content)
-
-    model_bytes = open(download_target, "rb").read()
-    if hashlib.sha256(model_bytes).hexdigest() != expected_sha256:
-        raise RuntimeError(
-            "Model has been downloaded but the SHA256 checksum does not not "
-            "match. Please retry loading the model."
-        )
-
-    return model_bytes if in_memory else download_target
+import numpy as np
 
 
 class WhisperTranscriber(BaseTranscriber):
@@ -69,6 +24,9 @@ class WhisperTranscriber(BaseTranscriber):
     This class provides an interface for OpenAI Whisper for off-line (local) 
     speech-to-text transcription in various languages. You must first download
     the model used for transcription on the local machine.
+
+    This interface uses `faster-whisper` for transcription, which is a faster 
+    version of the original `whisper` library provided by OpenAI.
 
     Parameters
     ----------
@@ -82,17 +40,20 @@ class WhisperTranscriber(BaseTranscriber):
     def __init__(self, initConfig=None):
         super(WhisperTranscriber, self).__init__(initConfig)
 
-        # pull in imports
-        import whisper
-        whisper._download = _download  # patch download func using `requests`
-
         initConfig = {} if initConfig is None else initConfig
 
-        self._device = initConfig.get('device', 'cpu')  # set the device
-        self._modelName = initConfig.get('model_name', 'base')  # set the model
+        self._device = initConfig.get('device', 'auto')  # set the device
+        self._modelName = initConfig.get('model_name', 'base.en')  # set the model
+        self._computeType = initConfig.get('compute_type', 'int8')
+
+        import faster_whisper
+        WhisperTranscriber.downloadModel(self._modelName)
 
         # setup the model
-        self._model = whisper.load_model(self._modelName).to(self._device)
+        self._model = faster_whisper.WhisperModel(
+            self._modelName, 
+            device=self._device, 
+            compute_type=self._computeType)
 
     @property
     def device(self):
@@ -122,9 +83,28 @@ class WhisperTranscriber(BaseTranscriber):
           installed on your system.
         
         """
-        import whisper
-        # todo - specify file name for the model
-        whisper.load_model(modelName)  # calling this downloads the model
+        import faster_whisper
+
+        # check if `modelName` is valid
+        if modelName not in WhisperTranscriber.getAllModels():
+            raise ValueError(
+                "Model `{}` is not available for download.".format(modelName))
+
+        # set the directory for the model, create if it doesn't exist
+        userModelDir = os.path.join(
+            prefs.paths['userPrefsDir'], 'cache', 'whisper')
+        if not os.path.isdir(userModelDir):
+            logging.info(
+                "Creating directory for Whisper models: {}".format(
+                    userModelDir))
+            os.mkdir(userModelDir)
+
+        # download the model
+        faster_whisper.download_model(
+            modelName, 
+            output_dir=userModelDir,
+            local_files_only=False  # avoid downloading if cached
+        )
 
     @staticmethod
     def getAllModels():
@@ -137,13 +117,13 @@ class WhisperTranscriber(BaseTranscriber):
 
         Returns
         -------
-        list 
-            List of available models.
+        tuple 
+            Sequence of available models.
 
         """
-        import whisper
+        from faster_whisper import utils
 
-        return whisper.available_models()
+        return utils._MODELS
     
     def transcribe(self, audioClip, modelConfig=None, decoderConfig=None):
         """Perform a speech-to-text transcription of a voice recording.
@@ -183,7 +163,7 @@ class WhisperTranscriber(BaseTranscriber):
             samples, samples.dtype).flatten().astype(np.float32)
         
         # resample if needed
-        if sr != 16000:
+        if int(sr) != 16000:
             import librosa
             waveform = librosa.resample(
                 waveform, 
@@ -191,8 +171,8 @@ class WhisperTranscriber(BaseTranscriber):
                 target_sr=16000)
         
         # pad and trim the data as required
-        import whisper.audio as _audio
-        waveform = _audio.pad_or_trim(waveform)
+        # import faster_whisper.audio as _audio
+        # waveform = _audio.pad_or_trim(waveform)
 
         modelConfig = {} if modelConfig is None else modelConfig
         decoderConfig = {} if decoderConfig is None else decoderConfig
@@ -203,30 +183,38 @@ class WhisperTranscriber(BaseTranscriber):
         word_timestamps = modelConfig.get('word_timestamps', True)
 
         # initiate the transcription
-        result = self._model.transcribe(
+        segments, _ = self._model.transcribe(
             waveform, 
             language=language, 
             temperature=temperature, 
             word_timestamps=word_timestamps,
             **decoderConfig)
-        
-        transcribed = result.get('text', '')
-        transcribed = transcribed.split(' ')  # split words 
-        language = result.get('langauge', '')
+        results = list(segments)
 
         # create the response value
-        toReturn = TranscriptionResult(
-            words=transcribed,
-            unknownValue=False,
-            requestFailed=False,
-            engine=self._engine,
-            language=language)
-        toReturn.response = str(result)  # provide raw JSON response
 
-        self.lastResult = toReturn
+        return results
         
-        return toReturn
+        # transcribed = result.get('text', '')
+        # transcribed = transcribed.split(' ')  # split words 
+        # language = result.get('langauge', '')
+
+        # # create the response value
+        # toReturn = TranscriptionResult(
+        #     words=transcribed,
+        #     unknownValue=False,
+        #     requestFailed=False,
+        #     engine=self._engine,
+        #     language=language)
+        # toReturn.response = str(result)  # provide raw JSON response
+
+        # self.lastResult = toReturn
+        
+        # return toReturn
 
 
 if __name__ == "__main__":
-    pass
+    print(WhisperTranscriber.getAllModels())
+    transc = WhisperTranscriber({'model_name': 'tiny.en'})
+
+    # test transcription
