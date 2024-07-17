42415546c8b6d8b0fa04278b34317afa1eca8486
Todd OST <todd.e.parsons@gmail.com>
2024-07-17 13:10:08 +0100

    BF: Handle registering backend in older PsychoPy versions

--- psychopy_whisper/components/whisperBackend.py
+++ psychopy_whisper/components/whisperBackend.py
@@ -7,4 +7,10 @@ from psychopy.experiment.components.microphone import MicrophoneComponent
 
 
 # register whisper backend with MicrophoneComponent
-MicrophoneComponent.localTranscribers['OpenAI Whisper'] = "Whisper"
+if hasattr(MicrophoneComponent.localTranscribers):
+    MicrophoneComponent.localTranscribers['OpenAI Whisper'] = "Whisper"
+else:
+    # if PsychoPy version predates this attribute (<2024.2.0), there should be a global instead
+    from psychopy.experiment.components.microphone import localTranscribers, allTranscribers
+    localTranscribers['OpenAI Whisper'] = "Whisper"
+    allTranscribers['OpenAI Whisper'] = "Whisper"
